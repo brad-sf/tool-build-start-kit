c820d69f910eff986a8e04cb20df2083562a2e2e
Bradley Flood <bradley.flood@gmail.com>
2015-04-08 08:45:37 +1000

    Back to basics running

--- public_html/index.php
+++ public_html/index.php
@@ -3,8 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <title>Title</title>
+  <link rel="stylesheet" type="text/css" href="/css/main.min.css">
 </head>
 <body>
+  <h1>Start</h1>
   
+  <script src="/js/main.min.js"></script>
 </body>
 </html>
